5aee8e0c7db71c89c5ef12140421cb7fd32aedda
Juhyeon Jeong <wwg6710@naver.com>
2024-04-18 19:42:14 +0900

    create 브랜치 생성 완료 and app_name = 'products' 까지 해놨음!

--- market/market/urls.py
+++ market/market/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 
+app_name = 'products'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
