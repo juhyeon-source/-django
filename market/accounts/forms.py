7a2281267cb4d15b68af54fbf846e7e7f2375d27
Juhyeon Jeong <wwg6710@naver.com>
2024-04-18 11:33:47 +0900

    회원가입 기능 구현 and 데코레이터까지 완료!

--- market/accounts/forms.py
+++ market/accounts/forms.py
@@ -5,3 +5,7 @@ class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
         fields = "__all__"
+
+class ArticleForm(forms.Form):
+    title = forms.CharField(max_length=10)
+    content = forms.CharField()
--- market/accounts/views.py
+++ market/accounts/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+from django.views.decorators.http import require_POST, require_http_methods
 
 def home(request):
     return render(request, 'accounts/home.html')
 
+@require_http_methods(["GET","POST"])
 def login(request):
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
@@ -17,8 +19,24 @@ def login(request):
     context = {'form' : form }
     return render(request, 'accounts/login.html', context)
 
+@require_POST
 def logout(request):
-    if request.method == "POST":
-        auth_logout(request)
+    auth_logout(request)
     return redirect("accounts:home")
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect("accounts:home")
+    else:
+        form = UserCreationForm()
+    context = {"form" : form}
+    return render(request, "accounts/signup.html", context)
+
+
+
+
 
