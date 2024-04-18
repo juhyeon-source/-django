82318bd1eb13a0b3ca6c0762d3c70012cc9d0ee5
Juhyeon Jeong <wwg6710@naver.com>
2024-04-18 21:25:36 +0900

    새 글 작성하고 작성한 글 목록에 보여주기까지 완료!

--- market/products/urls.py
+++ market/products/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = 'products'
 urlpatterns = [
     path('product_list/', views.product_list, name='product_list'),
+    path('new/', views.new, name='new'),
+    path('create/', views.create, name='create'),
+    
 ]
