0aeff148d8485a74e56197d22a33964b8e7f7dd2
Juhyeon Jeong <wwg6710@naver.com>
2024-04-19 00:49:24 +0900

    form 사용으로 간편하게 코딩하기!

--- market/products/urls.py
+++ market/products/urls.py
@@ -4,11 +4,9 @@ from . import views
 app_name = 'products'
 urlpatterns = [
     path('product_list/', views.product_list, name='product_list'),
-    path('new/', views.new, name='new'),
     path('create/', views.create, name='create'),
     path('<int:pk>/', views.product_detail, name='product_detail'),
     path('<int:pk>/delete/', views.delete, name='delete'),
-    path('<int:pk>/edit/', views.edit, name='edit'),
     path('<int:pk>/update/', views.update, name='update'),
-    
+
 ]
