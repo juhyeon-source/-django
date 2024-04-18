1de98c5d8ca184755550cf88a41caffe3bae21dc
Juhyeon Jeong <wwg6710@naver.com>
2024-04-18 16:45:37 +0900

    제품 목록 화면 구현 완료!

--- market/products/views.py
+++ market/products/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Article
 
 def product_list(request):
-    return render(request, 'product_list.html')
+    articles = Article.objects.all()
+    context = {"articles" : articles,}
+    return render(request, 'product_list.html', context)
